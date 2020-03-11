15c45f5ae65317bc345b41b49e37d4b6b5af4c07
Hanna Lee <mani21na@gmail.com>
2020-03-11 14:38:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution in this file!
+const s = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriverObj = {...driver};
+  newDriverObj[key] = value;
+}
