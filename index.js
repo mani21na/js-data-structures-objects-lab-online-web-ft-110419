ccb4e988d988b6f6290cab26874804d2682154a5
Hanna Lee <mani21na@gmail.com>
2020-03-11 14:57:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  const newDriverObj = Object.assign({}, driver);
+  newDriverObj = Object.assign({}, driver);
   newDriverObj[key] = value;
 
   return newDriverObj;
